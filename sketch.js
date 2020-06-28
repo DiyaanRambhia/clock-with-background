8ccf7292a05e58db5ca7c5b7c599e249b661d37d
DiyaanRambhia <67428235+DiyaanRambhia@users.noreply.github.com>
2020-06-28 20:51:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,103 +1,57 @@
-function setup() {
+var rainDrops = [];
 
-
+function setup() 
+{
 	createCanvas(400 , 400);
-	//createCanvas(800 , 400);
-	angleMode(DEGREES);
-
-
-
+	for (var i = 0; i < 300; i++) 
+	{
+		rainDrops[i] = new RainDrop();
+	}
 }
 
-
-
-
-
-function draw() {
-
+function draw() 
+{
 	background(0);
-
-	strokeWeight(4);
-	stroke(255);
-	noFill();
-	ellipse(200 , 200 , 300 , 300 );
-
-	fill(150 , 100 , 100);
-	textSize(21);
-	text('6' , 190 , 340);
-
-	fill(150 , 100 , 100);
-	textSize(21);
-	text('12' , 190 , 75);
-
-	fill(150 , 100 , 100);
-	textSize(21);
-	text('3' , 325 , 200);
-
-	fill(150 , 100 , 100);
-	textSize(21);
-	text('9' , 60 , 200);
-
-	var d = day();
-	var m = month();
-	var yr = year();
-
-	fill(255 , 255 , 150);
-	noStroke();
-	textSize(30);
-	text(d +'/'+m+'/'+yr , 10 , 390);
-
-
-	translate(200 , 200);
-	rotate(-90);
+	for (var i = 0; i < rainDrops.length; i++)
+	{
+		rainDrops[i].show();
+		rainDrops[i].move();
+	}	
+	
 
 	var hr = hour();
 	var min = minute();
-	var sec = second();
+	var sec = second();	
+
+	textSize(50);
+	stroke(255 , 150 , 100);
+	strokeWeight(1);
+	noFill()
+	text(hr + ':' + min + ':' + sec , 100 , 200);
 	
-	//fill(255);
-	//noStroke();
-	//text(hr + ':' + min +':' + sec , 10 , 200);
+}
 
-	strokeWeight(8);
-	stroke(255 , 100 , 150);
-	noFill();
-	let end1 = map(sec , 0 , 60 , 0 , 360);
-	//arc(0 , 0 , 300 , 300 , 0 , end1);
+function RainDrop()
+	{
+		this.x = random(0 , width);
+		this.y = random(-0 , -400);
+		
 
+	this.show = function()
+	{
+		stroke(255 , 151);
+		strokeWeight(4);
+		noFill();
+		rect(this.x , this.y , 0.00005 , 10);
+	}
+	this.move = function()
+	{
+		this.y = this.y + random(1 , 2);
 
-
-	stroke(150 , 100 , 255);
-	let end2 = map(min , 0 , 60 , 0 , 360);
-	//arc(0 , 0 , 280 , 280 , 0 , end2);
-
-
-	stroke(150 , 255 , 100);
-	let end3 = map(hr % 12 , 0 , 12 , 0 , 360);
-	//arc(0 , 0 , 260 , 260 , 0 , end3);
-	
-
-	push();
-	rotate(end1);
-	stroke(255 , 100 , 150);
-	line(0 , 0 , 100 , 0);
-	pop();
-
-	push();
-	rotate(end2);
-	stroke(150 , 100 , 255);
-	line(0 , 0 , 100 , 0);
-	pop();
-
-	push();
-	rotate(end3);
-	stroke(150 , 255 , 100);
-	line(0 , 0 , 100 , 0);
-	pop();
-
-
-	stroke(0);
-	point(0 , 0);
-
-
+		if (this.y > 420) 
+		{
+			this.y = random(0 , -400);
+		}
+	}
+		
 }
